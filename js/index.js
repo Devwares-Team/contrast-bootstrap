e789a8fb64eb6fc80b1b364bbb57280dee44c325
unknown <mankindofficial1@gmail.com>
2021-01-11 14:24:52 +0100

    updating

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import { Accordion } from "./accordion.js";
+import Popper from "./popper.min.js";
 import DataTable from "./data-tables.js";
 import { IFrame } from "./iframe.js";
 import { Rating } from "./rating.js";
 
-export { Accordion, DataTable, IFrame, Rating };
+export { Accordion, DataTable, IFrame, Popper, Rating };
